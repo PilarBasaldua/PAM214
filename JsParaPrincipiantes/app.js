5e52adae93ce38d0996449c036ecdc1eaeac65c1
PilarBasaldua <124049109@upq.edu.mx>
2025-09-10 18:50:53 -0600

    Creamos la estructura base del proyecto con HTML y enlace a app.js

---
+++ JsParaPrincipiantes/app.js
@@ -0,0 +1,15 @@
+document.write("<h1>Hello world</h1>");
+
+//Tipos de datos
+"Hello world"//string
+'Hello world'//string
+
+10000000 //number
+--2.3 
+
+//boolean
+true
+false 
+
+// array 
+['jose', 'ryan', 'martha']
