1194a051df4ba00e982327a4b9955686afdd6654
PilarBasaldua <124049109@upq.edu.mx>
2025-09-17 22:08:01 -0600

    Agregar arrow function saludoPersonalizado con nombre y edad

--- Practica1/Ejercicios.js
+++ Practica1/Ejercicios.js
@@ -16,3 +16,12 @@ const cuadrado = numero => numero * numero;
 console.log(cuadrado(2));  // 4
 console.log(cuadrado(5));  // 25
 console.log(cuadrado(10)); // 100
+
+
+/* Crea una arrow function llamada saludoPersonalizado que reciba dos parámetros: nombre y edad, y retorne una cadena como la siguiente*/
+//"Hola, me llamo Isay y tengo 37 años. "
+const saludoPersonalizado = (nombre, edad) => {
+    return "Hola, me llamo " + nombre + " y tengo " + edad + " años.";
+};
+
+console.log(saludoPersonalizado("Isay", 37));
