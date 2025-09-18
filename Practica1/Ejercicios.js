fa46738140595d3f3169590e322516e8003b02dc
PilarBasaldua <124049109@upq.edu.mx>
2025-09-17 19:02:51 -0600

    Convertir función cuadrado a arrow function

--- Practica1/Ejercicios.js
+++ Practica1/Ejercicios.js
@@ -7,3 +7,12 @@ const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
 //Muestra en consola en contenido de saludo 
 
 console.log(saludo)
+
+
+
+
+const cuadrado = numero => numero * numero;
+
+console.log(cuadrado(2));  // 4
+console.log(cuadrado(5));  // 25
+console.log(cuadrado(10)); // 100
