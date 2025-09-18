5d4a9e7962612e5e2f9aedf5eef0bd494418bce8
PilarBasaldua <124049109@upq.edu.mx>
2025-09-17 18:54:21 -0600

    Actualizar saludo con let y const

---
+++ Practica1/Ejercicios.js
@@ -0,0 +1,9 @@
+let nombre = "Armando";
+const edad = 25;
+
+nombre = "Ana Maria";
+
+const saludo = "Hola, " + nombre + ". Tienes " + edad + " años.";
+//Muestra en consola en contenido de saludo 
+
+console.log(saludo)
