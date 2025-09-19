3791cb5eed23f24c5374c5c18b4065f7ff717da8
PilarBasaldua <124049109@upq.edu.mx>
2025-09-19 17:19:46 -0600

    find, forEach y reduce

--- Practica1/Practica2.js
+++ Practica1/Practica2.js
@@ -25,3 +25,25 @@ const nombres = productos
     .map(producto => producto.nombre);
 
 console.log(nombres); //["Laptop", Monitor""]
+
+
+//Ejercicio 3
+const personas = [
+    { nombre: "Ana", edad: 22},
+    { nombre: "Luis", edad: 35},
+    { nombre: "María", edad: 28}
+];
+//Tu código aquí
+
+// 1.Biscar a la persona con nombre "Luis"
+const personaLuis = personas.find(persona => persona.nombre === "Luis");
+console.log("persona encontrada: ", personaLuis);
+
+//2.Imprimir el nombre de cada persona con su edad 
+personas.forEach(persona => {
+    console.log(persona.nombre + "tiene" + persona.edad + "años");
+});
+
+//3. Sumar todas las edades 
+const totalEdades = personas.reduce((acumulador, persona) => acumulador + persona.edad, 0);
+console.log("Suma total de edades: ", totalEdades);
