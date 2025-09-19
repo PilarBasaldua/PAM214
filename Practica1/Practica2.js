fb6ee4d591888ceaf999d48fb2b2b10e42fe8bcc
PilarBasaldua <124049109@upq.edu.mx>
2025-09-19 17:01:54 -0600

    Filtrar productos con precio mayor a 1000 y mostrar nombres

--- Practica1/Practica2.js
+++ Practica1/Practica2.js
@@ -11,3 +11,17 @@ const persona = {
 const {nombre, edad, direccion: {ciudad } } = persona;
 //Imprime el mensaje
 console.log("Me llamo " + nombre + ", tengo " + edad + " años y vivo en " + ciudad + ".");
+
+// Ejercicio 2
+const productos = [
+    { nombre: "Laptop", precio: 12000 },
+    { nombre: "Mouse", precio: 250},
+    { nombre: "Teclado", precio: 750},
+    { nombre: "Monitor", precio: 3000}
+];
+//Tu código aquí 
+const nombres = productos 
+    .filter(producto => producto.precio > 1000)
+    .map(producto => producto.nombre);
+
+console.log(nombres); //["Laptop", Monitor""]
